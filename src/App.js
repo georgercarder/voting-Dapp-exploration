1db68faccbda2d4b19641b88d9b52922d2e765e4
George Carder <georgercarder@gmail.com>
2018-02-23 19:07:29 -0800

    new logo.svg is a silly voting box. changed header color to be like FB just because

--- src/App.js
+++ src/App.js
@@ -7,12 +7,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello Voting Dapp</h1>
+					<img className='App-logo' src={logo} alt='logo'  />					
+          <h1 className="App-title">BlockChain Election</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
