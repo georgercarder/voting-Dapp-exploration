303f5d69914861ebee0254c035b3fd814db32dbf
George Carder <georgercarder@gmail.com>
2018-02-23 16:24:24 -0800

    built struct Voter in VotingContract.sol

--- contracts/VotingContract.sol
+++ contracts/VotingContract.sol
@@ -4,7 +4,14 @@ pragma solidity ^0.4.16;
 
 contract Ballot {
 
-
+  // new complex type. will be used for variables later
+  // It will represent a single voter.
+  struct Voter {
+    uint weight;  // weight is accumulated by delegation
+    bool voted;  // if true, that person already voted
+    address delegate;  // person delegated to
+    uint vote;  //index of the voted proposal  
+  }
 
 
 
