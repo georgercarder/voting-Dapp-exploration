baefb1cf61f240a8f1985fb63ec10f890373051d
George Carder <georgercarder@gmail.com>
2018-02-23 18:13:20 -0800

    new function delegate in VotingContract.sol

--- contracts/VotingContract.sol
+++ contracts/VotingContract.sol
@@ -61,6 +61,44 @@ contract Ballot {
     voters[voter].weight = 1;  
   }
 
+  /// Delegate your vote to the voter 'to'.
+  function delegate(address to) public {
+    // assigns reference
+    Voter storage sender = voters[msg.sender];
+    require(!sender.voted);
 
+    // Self-delegation is not allowed.
+    require(to != msg.sender);
+
+    // Forward the delegation as long as
+    // 'to' also delegated.
+    // In general, such loops are very dangerous,
+    // because if they run too long, they might
+    // need more gas than is available in a block.
+    // In this case, the delegation will not be execuded,
+    // but in other situations, such loops might
+    // cause a contract to get 'stuck' completely.
+    while (voters[to].delegate != address(0)) {
+      to = voters[to].delegate;
+
+      // We found a loop in the delegation, not allowed.
+      require(to != msg.sender);
+    }  
+
+    // Since 'sender' is a reference, this
+    // modifies 'voters[msg.sender].voted'
+    sender.voted = true;
+    sender.delegate = to;
+    Voter storage delegate = voters[to];
+    if (delegate.voted) {
+      // If the delegate already voted,
+      // directly add to the number of votes
+      proposals[delegate.vote].voteCount += sender.weight;
+    } else {
+      // If the delegate did not vote yet,
+      // add to her weight.
+      delegate.weight += sender.weight;
+    }
+  }
 
 }
